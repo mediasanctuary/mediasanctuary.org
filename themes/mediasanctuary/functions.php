1192a695d787a101bae4a0d238870aeeda3e6eb4
Dan Phiffer <dan@phiffer.org>
2021-02-08 23:05:28 -0500

    add functions.php and main.js

---
+++ themes/mediasanctuary/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+add_filter('show_admin_bar', '__return_false');
+
+add_action('after_setup_theme', function() {
+	add_theme_support('title-tag');
+});
+
+add_action('wp_enqueue_scripts', function() {
+	list($src, $version) = get_asset_url('js/main.js', true);
+	wp_enqueue_script('main', $src, array(
+		'jquery'
+	), $version);
+});
+
+function asset_url($file) {
+	echo get_asset_url($file);
+}
+
+function get_asset_url($file, $return_version = false) {
+	$url  = get_stylesheet_directory_uri() . "/$file";
+	$path = get_stylesheet_directory() . "/$file";
+	$ver  = filemtime($path);
+	if ($return_version) {
+		return array($url, $ver);
+	} else {
+		return "$url?$ver";
+	}
+}
