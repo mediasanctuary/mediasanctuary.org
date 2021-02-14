a798cbe6d4360658d164219c0b1e8dfd93f84f84
Dan Phiffer <dan@phiffer.org>
2021-02-13 22:39:21 -0500

    beginnings of navigation (redux)

--- themes/mediasanctuary/header.php
+++ themes/mediasanctuary/header.php
@@ -3,7 +3,19 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="<?php asset_url('dist/main.css'); ?>">
+		<link rel="shortcut icon" href="<?php asset_url('img/favicon.ico'); ?>">
 		<?php wp_head(); ?>
 	</head>
 	<body>
+		<nav class="top-nav">
+			<div class="container">
+				<h1 class="logo"><a href="/"><img src="<?php asset_url('img/logo-720.png'); ?>" alt="The Sanctuary For Independent Media"></a></h1>
+				<a href="#" class="nav-link">Events</a>
+				<a href="#" class="nav-link">Initiatives</a>
+				<a href="#" class="nav-link">Get Involved</a>
+				<a href="#" class="nav-link">About</a>
+
+				<a href="#" class="donate">Donate</a>
+			</div>
+		</nav>
 		<div class="container">
