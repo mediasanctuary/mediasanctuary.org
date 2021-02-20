c7f5042fdcca383bd804a7f47ea169fddda17c2c
Dan Phiffer <dan@phiffer.org>
2021-02-20 12:34:25 -0500

    responsive navigation

--- themes/mediasanctuary/header.php
+++ themes/mediasanctuary/header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
 		<link rel="stylesheet" href="<?php asset_url('dist/main.css'); ?>">
 		<link rel="shortcut icon" href="<?php asset_url('img/favicon.ico'); ?>">
 		<?php wp_head(); ?>
@@ -10,11 +11,16 @@
 		<nav class="top-nav">
 			<div class="container">
 				<h1 class="logo"><a href="/"><img src="<?php asset_url('img/logo-720.png'); ?>" alt="The Sanctuary For Independent Media"></a></h1>
-				<a href="#" class="nav-link">Events</a>
-				<a href="#" class="nav-link">Initiatives</a>
-				<a href="#" class="nav-link">Get Involved</a>
-				<a href="#" class="nav-link">About</a>
-				<a href="#" class="donate">Donate</a>
+				<div class="nav-link-container">
+					<a href="#" class="nav-link nav-link--menu">Menu</a>
+					<div class="nav-links">
+						<a href="#" class="nav-link">Events</a>
+						<a href="#" class="nav-link">Initiatives</a>
+						<a href="#" class="nav-link">Get Involved</a>
+						<a href="#" class="nav-link">About</a>
+					</div>
+					<a href="#" class="nav-link nav-link--donate">Donate</a>
+				</div>
 			</div>
 		</nav>
 		<div class="heading">
