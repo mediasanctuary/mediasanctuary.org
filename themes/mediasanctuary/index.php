f6358fa04e6ece101bd251b083a7d4647e4e8a59
Dan Phiffer <dan@phiffer.org>
2021-02-20 17:50:54 -0500

    news section

--- themes/mediasanctuary/index.php
+++ themes/mediasanctuary/index.php
@@ -3,4 +3,5 @@
 get_header();
 get_template_part('section-events');
 get_template_part('section-initiatives');
+get_template_part('section-news');
 get_footer();
---
+++ themes/mediasanctuary/section-news.php
@@ -0,0 +1,26 @@
+<div id="news" class="news">
+	<div class="container">
+		<h2>News</h2>
+		<div class="news__about">
+			<p>The latest happenings at The Santuary for Independent Media. You can also view the archive or listen to WOOC 105.7 for more updates.</p>
+		</div>
+		<div class="news__list">
+			<a href="https://www.mediasanctuary.org/time-for-reckoning-on-wmht-examines-post-protest-response/" class="news__item">
+				<span class="news__date">October 26, 2020</span>
+				<h3>‘Time for Reckoning’ on WMHT examines post-protest response</h3>
+				<img src="https://www.mediasanctuary.org/wp-content/uploads/2020/06/Screenshot_2020-06-03-Jamaica-Miles-on-Black-Lives-Matter-Protests-in-the-Capital-District-e1603741352673.png" alt="">
+			</a>
+			<a href="https://www.mediasanctuary.org/early-voting-opens-saturday-with-surge-anticipated-at-local-polls/" class="news__item">
+				<span class="news__date">October 25, 2020</span>
+				<h3>Early voting opens Saturday with surge anticipated at local polls</h3>
+				<img src="https://www.mediasanctuary.org/wp-content/uploads/2020/10/Image5.jpeg" alt="">
+			</a>
+			<a href="https://www.mediasanctuary.org/troy-organizations-provide-free-buses-for-early-voters/" class="news__item">
+				<span class="news__date">October 25, 2020</span>
+				<h3>Troy organizations provide free buses for early voters</h3>
+				<img src="https://www.mediasanctuary.org/wp-content/uploads/2020/10/120069850_10158967175348746_8209873448639366946_o-e1603228955471.jpg" alt="">
+			</a>
+		</div>
+		<br class="clear">
+	</div>
+</div>
