dba0950a7f2583c1713934004b11d236a5c265e3
Dan Phiffer <dan@phiffer.org>
2021-03-07 14:24:42 -0500

    make 'posts' read as 'Podcasts'

--- wp/wp-content/themes/mediasanctuary/functions.php
+++ wp/wp-content/themes/mediasanctuary/functions.php
@@ -27,7 +27,35 @@ add_action('admin_enqueue_scripts', function() {
 	$dir = get_template_directory();
 	$css_src = get_template_directory_uri() . '/css/admin/admin.css';
 	$css_version = filemtime("$dir/css/admin/admin.css");
-	wp_enqueue_style('custom-admin', $css_src, array(), $css_version);
+	wp_enqueue_style('custom-admin', $css_src, [], $css_version);
+});
+
+// Change the 'post' type to be labeled 'Podcasts'
+add_action('admin_menu', function() {
+	global $menu;
+	global $submenu;
+	$menu[5][0] = 'Podcasts';
+	$submenu['edit.php'][5][0] = 'Podcasts';
+	$submenu['edit.php'][10][0] = 'Add Podcast Post';
+});
+
+add_action('init', function() {
+	global $wp_post_types;
+	$labels = &$wp_post_types['post']->labels;
+	$labels->name = 'Podcasts';
+	$labels->singular_name = 'Podcast Post';
+	$labels->add_new = 'Add Podcast Post';
+	$labels->add_new_item = 'Add New Podcast Post';
+	$labels->edit_item = 'Edit Podcast Post';
+	$labels->new_item = 'Podcasts';
+	$labels->view_item = 'View Podcast Post';
+	$labels->search_items = 'Search Podcasts';
+	$labels->not_found = 'No Podcast Posts found';
+	$labels->not_found_in_trash = 'No Podcast Posts found in Trash';
+	$labels->all_items = 'All Podcast Posts';
+	$labels->menu_name = 'Podcasts';
+	$labels->name_admin_bar = 'Podcasts';
+	$wp_post_types['post']->menu_icon = 'dashicons-microphone';
 });
 
 function asset_url($file) {
