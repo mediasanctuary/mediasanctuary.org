f61313beb22e02ae6112eb7a5af82b7eb5b40159
Dan Phiffer <dan@phiffer.org>
2021-03-07 12:25:04 -0500

    load acf fields from 'acf'

--- wp/wp-content/themes/mediasanctuary/functions.php
+++ wp/wp-content/themes/mediasanctuary/functions.php
@@ -15,6 +15,10 @@ add_action('acf/init', function() {
 	require_once('blocks/blocks.php');
 });
 
+add_filter('acf/settings/load_json', function($path) {
+	return get_stylesheet_directory() . '/acf';
+});
+
 add_filter('acf/settings/save_json', function($path) {
 	return get_stylesheet_directory() . '/acf';
 });
