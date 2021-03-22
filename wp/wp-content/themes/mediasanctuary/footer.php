22aef9cb0380482f02317c4af333b9875f3f6fbd
Matthew Stellato <matthew@matthewstellato.com>
2021-03-22 00:09:26 -0400

    Getting organized. Footer and About section styling.

--- wp/wp-content/themes/mediasanctuary/footer.php
+++ wp/wp-content/themes/mediasanctuary/footer.php
@@ -1,4 +1,60 @@
 		</div>
+		
+    <section id="about">
+      <div class="container">
+        <div class="two-col">
+          
+          <div class="col w45">
+            <div class="initiatives">
+              <img src="<?php asset_url('img/Sanctuary-White-Black-Shadow.svg'); ?>" alt="" />
+              <ul>
+                <li><a href="/initiatives/collardcitygrowers/">Collard City Growers</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/freedom-square/">Freedom Square</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/kitchensanctuary/">KitchenSanctuary</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/nature-lab/">NATURE Lab Environmental Education Center</a></li>
+                <li><a href="https://www.mediasanctuary.org/sanctuary-tv/">Sanctuary TV</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/wooc/">WOOC 105.3 FM</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/uptownsummer/">Uptown Summer</a></li>
+                <li><a href="https://www.mediasanctuary.org/initiatives/youth-media-sanctuary/">Youth Media Sanctuary</a></li>
+              </ul> 
+            </div>           
+          </div>
+
+          <div class="col w55">
+            <h2>About The Sanctuary</h2>
+            <p>We use art and participatory action to promote social and environmental justice and freedom of creative expression.</p>
+            <a href="" class="btn">Learn More</a>
+          </div>
+
+        </div>
+      </div>
+    </section>		
+		
+		<footer>
+  		<div class="container">
+        <div class="two-col">  
+          <div class="col w45">      		
+        		<ul class="social">
+    					<li><a href="https://www.facebook.com/mediasanctuary" target="_blank" class="fb">Facebook</a></li>
+    					<li><a href="https://www.instagram.com/mediasanctuary" target="_blank" class="ig">Instagram</a></li>
+    					<li><a href="https://www.twitter.com/mediasanctuary" target="_blank" class="tw">Twitter</a></li>
+    					<li><a href="https://www.youtube.com/channel/UC5JTfy24J7STy5lwDeNHq6g" target="_blank" class="yt">YouTube</a></li>
+    					<li><a href="https://www.vimeo.com/mediasanctuary" target="_blank" class="vimeo">Vimeo</a></li>
+        		</ul>			
+          </div>
+          <div class="col w55">									      		
+            <p>&copy; 2020 <strong>&nbsp;The Sanctuary for Independent Media</strong></p>
+            <address>
+              3361 6th Avenue in North Troy, New York<br/>
+              P.O. Box 35 Troy, NY 12181<br/>
+              (518) 272-2390
+            </address>
+          </div>
+        </div>
+      </div>
+		</footer>
+		
 		<?php wp_footer(); ?>
+		
 	</body>
 </html>
---
+++ wp/wp-content/themes/mediasanctuary/page-templates/home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Home Page
+ * Description: The template used to display the Homepage
+ *
+ * @package WordPress
+ * @subpackage The Sanctuary for Indepeendent Media
+ * @since Media Sanctuary 2.0
+ */
+
+get_header();
+
+the_content();
+
+get_footer();
