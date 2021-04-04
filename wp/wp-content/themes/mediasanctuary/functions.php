497d895da6b39117c348836686b49467c57bd0bb
Dan Phiffer <dan@phiffer.org>
2021-04-04 10:22:05 -0400

    add podcasts post type

--- wp/wp-content/themes/mediasanctuary/functions.php
+++ wp/wp-content/themes/mediasanctuary/functions.php
@@ -40,6 +40,11 @@ add_action('admin_menu', function() {
 });
 
 add_action('init', function() {
+	news_post_type();
+	podcasts_post_type();
+});
+
+function news_post_type() {
 	global $wp_post_types;
 	$labels = &$wp_post_types['post']->labels;
 	$labels->name = 'News';
@@ -56,7 +61,38 @@ add_action('init', function() {
 	$labels->menu_name = 'News';
 	$labels->name_admin_bar = 'News';
 	$wp_post_types['post']->menu_icon = 'dashicons-welcome-widgets-menus';
-});
+}
+
+function podcasts_post_type() {
+	$labels = [
+		'name'               => 'Podcasts',
+		'singular_name'      => 'Podcast Episode',
+		'add_new'            => 'Add New Podcast Episode',
+		'add_new_item'       => 'Add New Podcast Episode',
+		'edit_item'          => 'Edit Podcast Episode',
+		'new_item'           => 'Podcast Episode',
+		'view_item'          => 'View Podcast Episode',
+		'search_items'       => 'Search Podcasts',
+		'not_found'          => 'No Podcast Episodes found',
+		'not_found_in_trash' => 'No Podcast Episodes found in Trash',
+		'all_items'          => 'All Podcast Episodes',
+		'menu_name'          => 'Podcasts',
+		'name_admin_bar'     => 'Podcasts'
+	];
+
+	register_post_type(
+		'podcasts',
+		array(
+			'labels'        => $labels,
+			'public'        => true,
+			'show_ui'       => true,
+			'menu_position' => 3,
+			'menu_icon'     => 'dashicons-microphone',
+			'supports'      => ['title', 'editor', 'thumbnail'],
+			'taxonomies'    => [],
+		)
+	);
+}
 
 function asset_url($file) {
 	echo get_asset_url($file);
