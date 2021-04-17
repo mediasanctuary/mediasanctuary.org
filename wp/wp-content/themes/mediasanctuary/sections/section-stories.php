a337505a146ff1ec538cc8d713c7427ad8b1ea71
Matthew Stellato <matthew@matthewstellato.com>
2021-04-17 10:55:31 -0400

    Adding custom fields to homepage

--- wp/wp-content/themes/mediasanctuary/sections/section-stories.php
+++ wp/wp-content/themes/mediasanctuary/sections/section-stories.php
@@ -1,7 +1,7 @@
 <section id="stories" class="posts p60">
 	<div class="container">
 		<h2>Recent Stories</h2>
-		<p>Much of the coverage from <a href="https://www.mediasanctuary.org/podcast-categories/hudson-mohawk-magazine/">Hudson Mohawk Magazine</a> that airs on WOOC 105.3 can also be found on our site. </p>
+		<p class="intro">Much of the coverage from <a href="/stories/">Hudson Mohawk Magazine</a> that airs on WOOC 105.3 can also be found on our site. </p>
 
 		<ul class="four-col">
 
@@ -21,6 +21,6 @@
   		?>
 
 		</ul>
-		<a href="<?php echo home_url('/stories/'); ?>">View all stories</a>
+		<p class="text-center"><a href="<?php echo home_url('/stories/'); ?>" class="btn">View all stories</a></p>
 	</div>
 </section>
