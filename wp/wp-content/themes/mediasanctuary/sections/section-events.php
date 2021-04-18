65bf7b8ba9f0fab686e57f7b76668df249fdf7a3
Matthew Stellato <matthew@matthewstellato.com>
2021-04-18 09:37:26 -0400

    Connecting Events to the homepage. New project template.

--- wp/wp-content/themes/mediasanctuary/sections/section-events.php
+++ wp/wp-content/themes/mediasanctuary/sections/section-events.php
@@ -2,37 +2,45 @@
 	<div class="container">
 		<h2>Upcoming Events</h2>
 		<p>We aren’t physically gathering at the Sanctuary for most events – but that doesn’t mean our programming has stopped! Check out our <a href="/events/">full schedule</a> of upcoming events.</p>
+				
 		<ul class="posts__list three-col">
-			<li class="col">
-				<a href="https://www.mediasanctuary.org/time-for-reckoning-on-wmht-examines-post-protest-response/" class="posts__item">
-					<span class="posts__date">Through April 1, 2021</span>
-					<span class="post__thumbnail">
-						<img src="https://www.mediasanctuary.org/wp-content/uploads/2020/12/nggallery_import/Altar_Heal_Our_Wounded_IMG_0421-630x440.jpg" alt="">
-						<strong>Learn More</strong>
-					</span>
-					<h5>Memorial Altar: "Emerging from Darkness"</h5>
-				</a>
-			</li>
-			<li class="col">			
-				<a href="https://www.mediasanctuary.org/early-voting-opens-saturday-with-surge-anticipated-at-local-polls/" class="posts__item">
-					<span class="posts__date">February 27, 2021</span>
-					<span class="post__thumbnail">
-						<img src="https://www.mediasanctuary.org/wp-content/uploads/2021/02/Kadir_Nelson_Resist_painting-630x440.jpg" alt="">
-						<strong>Learn More</strong>
-					</span>						
-					<h5>Black History: Capital Region Youth Rise Up</h5>
-				</a>
-				</li>
-			<li class="col">			
-				<a href="https://www.mediasanctuary.org/troy-organizations-provide-free-buses-for-early-voters/" class="posts__item">
-					<span class="posts__date">March 4, 2021</span>
-					<span class="post__thumbnail">					
-						<img src="https://www.mediasanctuary.org/wp-content/uploads/2021/02/hospitalconsolidationfinal1.jpg" alt="">
-						<strong>Learn More</strong>
-					</span>												
-					<h5>People’s Health Sanctuary presents: Community Forum to Address Capital Region Health System Merger</h5>
-				</a>
-			</li>
+
+  		<?php
+        $args = array(
+          'posts_per_page' => 3,
+          'post_type' => 'tribe_events'
+        );
+        $queryLatest = new WP_Query($args);
+        if ($queryLatest->have_posts()) :
+            $i = 0;
+            while ($queryLatest->have_posts()) : $queryLatest->the_post();
+
+          	$thumb_url = get_asset_url('img/default.jpg');
+          	if ( has_post_thumbnail() ) {
+          		$thumb_id = get_post_thumbnail_id();
+          		$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+          		$thumb_url = $thumb_url_array[0];
+          	}
+            
+            ?>
+            
+      			<li class="col">			
+              <a href="<?php the_permalink();?>" class="posts__item">
+      					<span class="posts__date"><?php the_time('F d, Y');?></span>
+                  <span class="post__thumbnail" style="background-image:url(<?php echo $thumb_url; ?>)">
+      						<strong>Learn More</strong>
+      					</span>												
+                <h5><?php the_title();?></h5>
+      				</a>
+      			</li>
+
+            <?php endwhile;
+        endif;
+        wp_reset_query();
+  		?>
+
 		</ul>
+		
+		
 	</div>
 </section>
