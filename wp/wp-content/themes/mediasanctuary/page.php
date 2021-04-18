bd838759f15b36fedf943b782193c4ec1c900abb
Matthew Stellato <matthew@matthewstellato.com>
2021-04-18 18:27:11 -0400

    Remove duplicate Title

--- wp/wp-content/themes/mediasanctuary/page.php
+++ wp/wp-content/themes/mediasanctuary/page.php
@@ -3,10 +3,9 @@
 	<div class="container">
 			<?php
 			if(have_posts()) : while(have_posts()) : the_post(); 
-			    echo '<h1>'; the_title(); echo '</h1>';
 		    	the_content();
 		    endwhile; endif;
-		    ?>
+		  ?>
 	</div>
 </section>
 <?php get_footer(); ?>
