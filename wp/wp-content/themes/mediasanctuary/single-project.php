f353b81f0712b8cc4165a24d1ae91a14f797ba92
Matthew Stellato <matthew@matthewstellato.com>
2021-04-19 21:14:22 -0400

    Integrate title in header on Project posts

--- wp/wp-content/themes/mediasanctuary/single-project.php
+++ wp/wp-content/themes/mediasanctuary/single-project.php
@@ -3,12 +3,6 @@
   if (have_posts()) : while(have_posts()) : the_post();
 ?>
 
-<section class="pageHeading">
-  <div class="wrap">
-    <h1><?php the_title();?></h1>
-  </div>
-</section>
-
 <section class="p40">
   <div class="container sm">
     <?php the_content();?>
