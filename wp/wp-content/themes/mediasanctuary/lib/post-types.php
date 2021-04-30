fa233273a84b3ede9927594ebab6fc145e0e3286
Matthew Stellato <matthew@matthewstellato.com>
2021-04-30 18:34:36 -0400

    Hotfix: Resolve Initiative project archive pages

--- wp/wp-content/themes/mediasanctuary/lib/post-types.php
+++ wp/wp-content/themes/mediasanctuary/lib/post-types.php
@@ -72,6 +72,7 @@ function initiatives_taxonomy() {
 		'show_admin_column'  => true,
 		'show_in_nav_menus'  => true,
 		'show_in_rest'       => true,
-		'show_tagcloud'      => false
+		'show_tagcloud'      => false,
+		'rewrite'            => array( 'slug' => 'projects' )
 	]);
 }
